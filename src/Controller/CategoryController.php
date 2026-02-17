83e189bba38cd2d9f0e87e58b8dfaba331e1c0e2
jan <jan.zdan1234@gmail.com>
2026-02-17 13:44:00 +0100

    Add getAllCategories method to CategoryController for retrieving all categories

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,12 +2,20 @@
 
 namespace App\Controller;
 
+use App\Service\CategoryService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 final class CategoryController extends AbstractController
 {
     protected function getReadGroup(): string
     {
         return 'category:read';
+    }
+
+    #[Route('/categories', name: 'app_category_get_all', methods: ['GET'])]
+    public function getAllCategories(CategoryService $service): JsonResponse
+    {
+
     }
 }

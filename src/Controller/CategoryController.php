2f1193712b2a358894f35013025d6cd76e6b7044
jan <jan.zdan1234@gmail.com>
2026-02-19 17:50:02 +0100

    Refactor getAllCategories method in CategoryController to use CategoryRepository for retrieving categories

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use App\Service\CategoryService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
 
 final class CategoryController extends AbstractController
 {
@@ -14,8 +16,11 @@ final class CategoryController extends AbstractController
     }
 
     #[Route('/categories', name: 'app_category_get_all', methods: ['GET'])]
-    public function getAllCategories(CategoryService $service): JsonResponse
+    public function getAllCategories(CategoryRepository $repository): JsonResponse
     {
+        $categories = $repository->findAll();
 
+        return $this->json($categories, context: ['groups' => $this->getReadGroup()]);
     }
+
 }

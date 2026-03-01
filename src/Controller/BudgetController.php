1bb6b28dc6b27c8bce2db79a17ac992ddcebbe75
jan <jan.zdan1234@gmail.com>
2026-03-01 15:44:19 +0100

    Add new method to list all budgets in a BudgetController.php

--- src/Controller/BudgetController.php
+++ src/Controller/BudgetController.php
@@ -2,9 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\BudgetRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 class BudgetController extends AbstractController
 {
-
+    #[Route('', methods: ['GET'])]
+    public function list(BudgetRepository $repository): JsonResponse
+    {
+        return $this->json(
+            $repository->findAll(),
+            Response::HTTP_OK,
+            [],
+            ['groups' => ['category:read']]
+        );
+    }
 }

4a9eaac83d9f9f4e7386d1bffaf316ad631a1841
jan <jan.zdan1234@gmail.com>
2026-03-05 16:54:44 +0100

    Add validation constraints and constructor to MonthlyDto

--- src/Dto/Report/MonthlyDto.php
+++ src/Dto/Report/MonthlyDto.php
@@ -2,7 +2,21 @@
 
 namespace App\Dto\Report;
 
+use App\Enum\TransactionType;
+use Symfony\Component\Validator\Constraints as Assert;
 class MonthlyDto
 {
+    public function __construct(
+        #[Assert\NotBlank]
+        public int $month,
 
+        #[Assert\NotBlank]
+        public int $year,
+
+        #[Assert\NotBlank]
+        public ?array $categories = null,
+
+        #[Assert\NotBlank]
+        public ?TransactionType $filterType = null
+    ) {}
 }

aaf165c2bb9327f9ae7eba8de2d31ccc2ed9ef50
jan <jan.zdan1234@gmail.com>
2026-03-06 16:09:41 +0100

    Add monthly report endpoint to ReportController

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -2,9 +2,27 @@
 
 namespace App\Controller;
 
+use App\Dto\Report\MonthlyDto;
+use App\Service\ReportService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/reports')]
 class ReportController extends AbstractController
 {
+    /**
+     * @throws \DateMalformedStringException
+     */
+    #[Route('/monthly', name: 'app_report_monthly')]
+    public function monthlyReport(
+        #[MapRequestPayload] MonthlyDto $dto,
+        ReportService $service
+    ): JsonResponse
+    {
+        $report = $service->getMonthlyReport($dto->month, $dto->year, $dto->categories, $dto->filterType);
 
+        return $this->json($report);
+    }
 }

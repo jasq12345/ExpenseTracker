7c464be0e40076c3fda84ca3276375cc33c73424
jan <jan.zdan1234@gmail.com>
2026-03-18 15:58:37 +0100

    Add logic to BudgetService for handling missing current budget

--- src/Service/TransactionService.php
+++ src/Service/TransactionService.php
@@ -5,7 +5,10 @@ namespace App\Service;
 use App\Dto\Transaction\CreateTransactionDto;
 use App\Dto\Transaction\UpdateTransactionDto;
 use App\Entity\Transaction;
+use App\Enum\TransactionType;
+use App\Repository\BudgetRepository;
 use App\Repository\CategoryRepository;
+use App\Service\Notificaation\BudgetAlertService;
 use Doctrine\ORM\EntityManagerInterface;
 
 readonly class TransactionService
@@ -14,14 +17,46 @@ readonly class TransactionService
         private EntityManagerInterface $em,
         private CategoryRepository $categoryRepository,
         private UserProviderService $userProvider,
+        private BudgetRepository $budgetRepository,
+        private BudgetAlertService $budgetAlertService,
     ){}
 
     public function create(CreateTransactionDto $dto): Transaction
     {
         $user = $this->userProvider->getUser();
+        $budget = $this->budgetRepository->findCurrentBudgetByUser($user);
 
+        if(!$budget){
+            throw new \DomainException(
+                'No active budget for this month. Please create a budget before making transactions.'
+            );
+        }
+
+        $totalAmount = $dto->amount * $dto->price;
         $category = $this->categoryRepository->findOneByIdAndUser($dto->categoryId, $user);
 
+        $transaction = $this->createTransaction($dto);
+
+        $user->addTransaction($transaction);
+        $category->addTransaction($transaction);
+
+        match ($dto->type) {
+            TransactionType::EXPENSE => $budget->addExpense($totalAmount),
+            TransactionType::INCOME  => $budget->addIncome($totalAmount),
+        };
+
+        $this->em->persist($transaction);
+        $this->em->flush();
+
+        if ($dto->type === TransactionType::EXPENSE) {
+            $this->budgetAlertService->checkAndAlert($budget);
+        }
+
+        return $transaction;
+    }
+
+    private function createTransaction(CreateTransactionDto $dto): Transaction
+    {
         $transaction = new Transaction();
 
         $transaction->setName($dto->name);
@@ -29,12 +64,6 @@ readonly class TransactionService
         $transaction->setPrice($dto->price);
         $transaction->setDescription($dto->description);
         $transaction->setType($dto->type);
-
-        $user->addTransaction($transaction);
-        $category->addTransaction($transaction);
-
-        $this->em->persist($transaction);
-        $this->em->flush();
 
         return $transaction;
     }
